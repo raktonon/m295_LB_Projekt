c4e78794c07fee0400c0c957098d2df6a11787df
raktonon <s-r@gmx.ch>
2025-08-13 13:26:04 +0200

    fresh start

---
+++ src/main/java/com/wiss/cocktailbackend/CocktailBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wiss.cocktailbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wiss/cocktailbackend/CocktailBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiss.cocktailbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
