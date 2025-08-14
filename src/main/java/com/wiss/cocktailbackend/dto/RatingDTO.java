b76f029ec42496e48296e287070d267c5974dcc3
Ritter Sascha (rittesa3) <rittesa3@students.zhaw.ch>
2025-08-14 16:04:01 +0200

    update

--- src/main/java/com/wiss/cocktailbackend/dto/RatingDTO.java
+++
@@ -1,5 +0,0 @@
-package com.wiss.cocktailbackend.dto;
-
-import jakarta.validation.constraints.*;
-
-public record RatingDTO(@Min(1) @Max(5) int stars, @Size(max = 1000) String comment) {}
--- src/main/java/com/wiss/cocktailbackend/repository/RatingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wiss.cocktailbackend.repository;
-
-import com.wiss.cocktailbackend.entity.Rating;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RatingRepository extends JpaRepository<Rating, Long> {
-    long countByCocktail_Id(Long cocktailId);
-    java.util.List<Rating> findByCocktail_Id(Long cocktailId);
-}
